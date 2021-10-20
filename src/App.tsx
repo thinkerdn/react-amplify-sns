83c4ec5b8fd9c1dd29397e1284a6ac725bc1e83b
thinkerdn <thinkerdn@gmail.com>
2021-10-20 09:24:13 +0900

    0.0.1 初期コミット

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, createContext } from "react"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 
-function App() {
+import Amplify, { Auth } from "aws-amplify"
+import { AmplifyAuthenticator, AmplifySignUp } from "@aws-amplify/ui-react"
+import awsconfig from "./aws-exports"
+
+import Wrapper from "./components/layouts/Wrapper"
+import PostList from "./components/post/PostList"
+
+import { User } from "./types/index"
+
+export const UserContext = createContext({} as {
+  currentUser: User | undefined
+  setCurrentUser: React.Dispatch<React.SetStateAction<User | undefined>>
+})
+
+Amplify.configure(awsconfig)
+
+const App: React.FC = () => {
+  const [currentUser, setCurrentUser] = useState<User>()
+
+  const getCurrentUser = async () => {
+    const currentUserInfo = await Auth.currentUserInfo()
+    setCurrentUser(currentUserInfo)
+  }
+
+  useEffect(() => {
+    getCurrentUser()
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <AmplifyAuthenticator>
+      <AmplifySignUp
+        slot="sign-up"
+        formFields={[
+          { type: "username" },
+          { type: "email" },
+          { type: "password" }
+        ]}
+      >
+      </AmplifySignUp>
+      <Router>
+        <UserContext.Provider value={{ currentUser, setCurrentUser }}>
+          <Wrapper>
+            <Switch>
+              <Route exact path="/" component={PostList} />
+            </Switch>
+          </Wrapper>
+        </UserContext.Provider>
+      </Router>
+    </AmplifyAuthenticator>
+  )
 }
 
-export default App;
+export default App
